30bb9fcd5b70377349f308cc8e064787d5ede973
Victor Shaviya <vs610642@digicelgroup.com>
2023-02-17 15:08:06 -0500

    Refactor the server logic

--- server.js
+++ server.js
@@ -45,27 +45,34 @@ app.get('/todos', (req, res) => res.send(todos));
 app.post('/todos', (req, res) => {
 	const todo = { title: req.body.title, id: nanoid(), completed: false };
 	todos.push(todo);
+
 	return res.send(todo);
+
 });
 
 app.patch('/todos/:id', (req, res) => {
 	const id = req.params.id;
 	const index = todos.findIndex((todo) => todo.id == id);
 	const completed = Boolean(req.body.completed);
+
 	if (index > -1) {
 		todos[index].completed = completed;
 	}
+
 	return res.send(todos[index]);
+
 });
 
 app.delete('/todos/:id', (req, res) => {
 	const id = req.params.id;
 	const index = todos.findIndex((todo) => todo.id == id);
+
 	if (index > -1) {
 		todos.splice(index, 1);
 	}
 
 	res.send(todos);
+	
 });
 
 const PORT = 7000;
